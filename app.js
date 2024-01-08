dfbfcee41ccd3f0d6c7febb448e5d81d09c9a7af
sem0ark <arcadii.sem@gmail.com>
2024-01-08 13:42:04 +0100

    fix(advanced-1.2): add alt attribute to html file images and in the creation function in js file
    Add "alt" attribute to img tags in html and js files according to Advanved recommendations
    html-and-css-extended.md

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ var createNewTaskElement = function (taskString) {
 
   deleteButton.className = "delete";
   deleteButtonImg.src = "./remove.svg";
+  deleteButtonImg.alt = "remove";
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
